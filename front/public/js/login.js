2bb1428f975b803ff37ee0ca2dad52a0fdf9dfc4
baek <baekbr13@gmail.com>
2023-01-31 20:35:49 +0900

    fix: router 수정

--- front/public/js/login.js
+++ front/public/js/login.js
@@ -3,61 +3,10 @@ const request = axios.create({
     withCredentials: true,
 })
 
+const signUpButton = document.getElementById("signUp")
+const signInButton = document.getElementById("signIn")
+const container = document.getElementById("container")
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+signUpButton.addEventListener("click", () => container.classList.add("right-panel-active"))
 
-//login
-const loginForm = document.querySelector("#form")
-const logininput = document.querySelectorAll("#form > label >input")
-
-// join
-const userInfo = document.querySelector("#userinfo")
-const input = document.querySelectorAll("#userinfo > label > input")
-
-signUpButton.addEventListener('click', () =>
-    container.classList.add('right-panel-active'));
-
-signInButton.addEventListener('click', () =>
-    container.classList.remove('right-panel-active'));
-
-// login
-loginForm.addEventListener("submit", async (e) => {
-    e.preventDefault()
-    let loginValue = []
-    for (let i = 0; i < logininput.length; i++) {
-        loginValue.push(logininput[i].value)
-    }
-    console.log(loginValue)
-    const [userId, userPw] = loginValue
-
-})
-
-// join
-userInfo.addEventListener("submit", async (e) => {
-    e.preventDefault()
-    let userValue = []
-    for (let i = 0; i < input.length; i++) {
-        userValue.push(input[i].value)
-    }
-    const [userId, userPw, userName, nickName, address, gender, phoneNum, userEmail, userIntro, userPic] = userValue
-    const response = await axios.post("http://127.0.0.1:3000/user/join", {
-        userId,
-        userPw,
-        userName,
-        nickName,
-        address,
-        gender,
-        phoneNum,
-        userEmail,
-        userIntro,
-        userPic
-    }, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    console.log(response)
-
-})
+signInButton.addEventListener("click", () => container.classList.remove("right-panel-active"))
--- front/routes/index.js
+++ front/routes/index.js
@@ -1,10 +1,22 @@
 const express = require("express")
 const router = express.Router()
+const axios = require("axios")
+const request = axios.create({
+    baseURL: "http://127.0.0.1:3000",
+    withCredentials: true,
+})
 
 router.get("/", (req, res, next) => {
     res.render("index.html")
 })
-
+router.post("/user/join", async (req, res, next) => {
+    const response = await request.post("/user/join", {
+        ...req.body,
+    })
+    console.log(response.data)
+    const { userPic, userId, userPw, userName, nickName, address, gender, phoneNum, userEmail, userIntro } = response.data
+    res.render("user/welcome.html", { userPic, userId, userPw, userName, nickName, address, gender, phoneNum, userEmail, userIntro })
+})
 router.get("/user/login", (req, res, next) => {
     res.render("user/login.html")
 })
