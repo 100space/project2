ee48e52f9b8972106d57a6a98fcc9b6e7444137a
Hanjin <csa0545@gamil.com>
2023-01-31 18:52:51 +0900

    fix: user join feature

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const auth = require('../src/auth/auth.route');
 const user = require('../src/user/user.route');
 
-router.use("/auth", auth);
-router.use("/login", user);
+router.use("/auth", auth)
 router.use("/user", user);
 router.use("/profile", user);
 
