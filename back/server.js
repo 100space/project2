35d455838e08602afec007322728f34d0396bb4b
Hanjin <csa0545@gamil.com>
2023-01-31 14:23:59 +0900

    fix : join  format fix

--- back/server.js
+++ back/server.js
@@ -3,14 +3,14 @@ const port = process.env.PORT || '3000';
 const app = require('./app');
 const { sequelize } = require('./models');
 
-app.use((error,req,res,next) => {
+app.use((error, req, res, next) => {
     console.log(error);
     res.status(500).send(error.message);
 });
 
-app.listen(port, async()=>{
+app.listen(port, async () => {
     console.log('connecting to backend and Database...')
-    await sequelize.sync({force : true});
+    await sequelize.sync({ force: true });
     console.log(`Starting Server with port Number is ${port}`);
 });
 
