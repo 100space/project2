96bcb00e4f30e34885420d7f46f2c31bd7e5c994
baek <baekbr13@gmail.com>
2023-02-01 15:17:28 +0900

    feat: main 페이지

--- front/public/js/login.js
+++ front/public/js/login.js
@@ -6,8 +6,19 @@ const request = axios.create({
 const signUpButton = document.getElementById("signUp")
 const signInButton = document.getElementById("signIn")
 const container = document.getElementById("container")
+const input = document.querySelector("#userinfo > label > input")
 
 signUpButton.addEventListener("click", () => container.classList.add("right-panel-active"))
-
 signInButton.addEventListener("click", () => container.classList.remove("right-panel-active"))
 
+const idCheck = async (e) => {
+    let inputTag = e.target
+    console.dir(e.target)
+    if (inputTag.name === "userId") {
+        const response = await request.get(`/check/?${inputTag.name}=${inputTag.value}`)
+    }
+    if (inputTag.name === "nickName") {
+        const response = await request.get(`/check/?${inputTag.name}=${inputTag.value}`)
+    }
+}
+input.addEventListener("input", idCheck)
