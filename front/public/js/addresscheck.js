1ff76e2ded632b1aa67dbe7d384ec43c83de1d92
Hynn <choihwoong@me.com>
2023-02-02 12:48:08 +0900

    fix : code update : kakao

--- front/public/js/addresscheck.js
+++ front/public/js/addresscheck.js
@@ -1,82 +1,59 @@
 const element_layer = document.getElementById('layer');
+const element_access = document.getElementById('eventAddress');
 
     function closeDaumPostcode() {
-        // iframe을 넣은 element를 안보이게 한다.
         element_layer.style.display = 'none';
+        element_access.readOnly = true;
     }
 
     function sample2_execDaumPostcode() {
         new daum.Postcode({
             oncomplete: function(data) {
-                // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+                var addr = ''; 
+                var extraAddr = '';
 
-                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                var addr = ''; // 주소 변수
-                var extraAddr = ''; // 참고항목 변수
-
-                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                
+                if (data.userSelectedType === 'R') {
                     console.log('road',addr)
                     addr = data.roadAddress;
-                } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                } else { 
                     console.log('jibun',addr)
                     addr = data.jibunAddress;
                 }
-
-                // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
                 if(data.userSelectedType === 'R'){
-                    // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                    // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
                     if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
                         extraAddr += data.bname;
                     }
-                    // 건물명이 있고, 공동주택일 경우 추가한다.
                     if(data.buildingName !== '' && data.apartment === 'Y'){
                         extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                     }
-                    // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
                     if(extraAddr !== ''){
                         extraAddr = ' (' + extraAddr + ')';
                     }
-                    // 조합된 참고항목을 해당 필드에 넣는다.
                     document.getElementById("eventAddress").value = extraAddr;
-                
                 } else {
                     document.getElementById("eventAddress").value = '';
                 }
-
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
                 document.getElementById("eventAddress").value = addr;
-                // 커서를 상세주소 필드로 이동한다.
                 document.getElementById("inputAddress").focus();
-
-                // iframe을 넣은 element를 안보이게 한다.
-                // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
                 element_layer.style.display = 'none';
             },
             width : '100%',
             height : '100%',
             maxSuggestItems : 5
         }).embed(element_layer);
-
-        // iframe element display Setting
         element_layer.style.display = 'block';
-
-        // iframe을 넣은 element의 위치설정 function
         initLayerPosition();
     }
 
-    
     function initLayerPosition(){
-        const width = 500; //우편번호서비스가 들어갈 element의 width
-        const height = 600; //우편번호서비스가 들어갈 element의 height
-        const borderWidth = 1; // border의 두께
+        const width = 500;
+        const height = 600;
+        const borderWidth = 1;
 
         element_layer.style.width = width + 'px';
         element_layer.style.height = height + 'px';
         element_layer.style.border = borderWidth + 'px solid';
-        // 실행되는 순간 Layer 의 position
         element_layer.style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/1.5 - borderWidth) + 'px';
         element_layer.style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 'px';
     }
