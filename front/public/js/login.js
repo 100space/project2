4c3e3954629ec38dbdab4aadc500c95e1b9d5674
Hanjin <csa0545@gamil.com>
2023-02-02 12:14:45 +0900

    feat : nickname overlap checked

--- front/public/js/login.js
+++ front/public/js/login.js
@@ -10,8 +10,10 @@ const input = document.querySelector("#userinfo > label > input")
 const form = document.querySelector("#form")
 const idCheck = document.querySelector("input[name='userId']")
 const idOverlap = document.querySelector("label>p")
-// const idFocus = document.querySelector("input[name='userPw']")
+const idFocus = document.querySelector("input[name='userPw']")
 const nickCheck = document.querySelector("input[name='nickName']")
+const nickFocus = document.querySelector("input[name='address']")
+console.log(nickFocus)
 
 const joinFrm = document.querySelector("#userinfo")
 
@@ -30,34 +32,86 @@ joinFrm.addEventListener("input", async (e) => {
         })
         const { data } = response
         if (data) {
+            let pmakeForm = valueFocus.parentNode
+            let p = document.createElement("p")
+            p.innerHTML = "중복된 아이디가 존재합니다."
+            p.style.display = "block"
+            pmakeForm.appendChild(p)
 
-            valueFocus.lastC.innerHTML = "중복된 아이디가 존재합니다."
-            idOverlap.style.display = "block"
         } else {
-            idOverlap.innerHTML = ""
-            idOverlap.style.display = "none"
+            let pmakeForm = valueFocus.parentNode
+            let p = pmakeForm.lastChild
+            pmakeForm.removeChild(p)
         }
-    } else if (check === "userPw") {
-        valueFocus.addEventListener("input", (e) => {
-            if (idOverlap.innerHTML) {
-                valueFocus.value = ""
-                alert("아이디 중복값을 확인해주세요")
-                idCheck.focus()
-            }
-        })
     } else if (check === "nickName") {
-        console.log(checkValue)
-        const response = await request.post("/user/checkNick", {
-            nickName: checkValue
-        }, {
+        const response = await request.post("/user/checkNick",
+            {
+                nickName: checkValue
+            }, {
             headers: {
                 "Content-Type": "application/json"
             }
         })
         const { data } = response
+        if (data) {
+            let pmakeForm = valueFocus.parentNode
+            let p = document.createElement("p")
+            p.innerHTML = "중복된 닉네임이 존재합니다."
+            p.style.display = "block"
+            pmakeForm.appendChild(p)
+        } else {
+            let pmakeForm = valueFocus.parentNode
+            let p = pmakeForm.lastChild
+            pmakeForm.removeChild(p)
+        }
+    }
+    // else if (check === "userPw") {
+    //     valueFocus.addEventListener("input", (e) => {
+    //         if (idOverlap.innerHTML) {
+    //             valueFocus.value = ""
+    //             alert("아이디 중복값을 확인해주세요")
+    //             idCheck.focus()
+    //         }
+    //     })
+    // } else if (check === "nickName") {
+    //     console.log(checkValue)
+    //     const response = await request.post("/user/checkNick", {
+    //         nickName: checkValue
+    //     }, {
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }
+    //     })
+    //     const { data } = response
 
+    // }
+})
+
+joinFrm.addEventListener("change", async (e) => {
+    const valueFocus = e.target
+    const check = e.target.name
+    const checkValue = e.target.value
+
+})
+
+idFocus.addEventListener("focus", (e) => {
+    let idOver = idFocus.parentNode.previousSibling.previousSibling
+    let overValue = idOver.lastChild.innerHTML
+    let checkVal = "중복된 아이디가 존재합니다."
+    if (overValue === checkVal) {
+        idCheck.focus()
     }
 })
+
+nickFocus.addEventListener("focus", (e) => {
+    let nickOver = nickFocus.parentNode.previousSibling.previousSibling
+    let overValue = nickOver.lastChild.innerHTML
+    let checkVal = "중복된 닉네임이 존재합니다."
+    if (overValue === checkVal) {
+        nickCheck.focus()
+    }
+})
+
 
 form.addEventListener("submit", async (e) => {
     try {
