21c51b00137629be44484ed8eeb8a9dfccb3a861
BAEK'space <112856532+100space@users.noreply.github.com>
2023-02-17 14:17:14 +0900

    error

--- back/lib/jwt.js
+++ back/lib/jwt.js
@@ -8,33 +8,52 @@ class JWT {
     }
 
     Sign(data, options = {}) {
-        const header = this.encode({ tpy: "JWT", alg: "sha256" })
-        const payload = this.encode({ ...data, ...options })
-        console.log(payload)
-        const signature = this.createSignature([header, payload])
-        return [header, payload, signature].join(".")
+        try {
+            const header = this.encode({ tpy: "JWT", alg: "sha256" })
+            const payload = this.encode({ ...data, ...options })
+            console.log(payload)
+            const signature = this.createSignature([header, payload])
+            return [header, payload, signature].join(".")
+        } catch (e) {
+            throw new Error(e)
+        }
     }
 
     verify(token, salt) {
-        const [header, payload, signature] = token.split(".")
-        const newSignature = this.createSignature([header, payload], salt)
-        if (newSignature !== signature) throw new Error("Token is Invalid, Pleas check the your token")
+        try {
+            const [header, payload, signature] = token.split(".")
+            const newSignature = this.createSignature([header, payload], salt)
+            if (newSignature !== signature) throw new Error("Token is Invalid, Pleas check the your token")
 
-        return this.decode(payload)
+            return this.decode(payload)
+        } catch (e) {
+            throw new Error(e)
+        }
     }
 
     encode(obj) {
-        console.log(obj)
-        return Buffer.from(JSON.stringify(obj)).toString("base64url")
+        try {
+            return Buffer.from(JSON.stringify(obj)).toString("base64url")
+        } catch (e) {
+            throw new Error(e)
+        }
     }
 
     decode(base64url) {
-        return JSON.parse(Buffer.from(base64url, "base64url").toString("utf8"))
+        try {
+            return JSON.parse(Buffer.from(base64url, "base64url").toString("utf8"))
+        } catch (e) {
+            throw new Error(e)
+        }
     }
 
     createSignature(base64urls, salt = SALT) {
-        const data = base64urls.join(".")
-        return this.crypto.createHmac("sha256", salt).update(data).digest("base64url")
+        try {
+            const data = base64urls.join(".")
+            return this.crypto.createHmac("sha256", salt).update(data).digest("base64url")
+        } catch (e) {
+            throw new Error(e)
+        }
     }
 }
 
